5aad389282332d96b2f21c17edd98187105e2f13
Kalpesh Panchbhai <kpanchbhai@mdsol.com>
2021-10-27 21:08:54 +0530

    Added ArrayList and Linked definition

--- src/main/java/com/datastructure/linkedlist/LinkedListCaller.java
+++ src/main/java/com/datastructure/linkedlist/LinkedListCaller.java
@@ -1,4 +1,15 @@
 package com.datastructure.linkedlist;
 
+import java.util.LinkedList;
+
 public class LinkedListCaller {
+  public static void main(String[] args) {
+    LinkedListImp<Integer> linkedListImp = new LinkedListImp<>();
+    linkedListImp.addFirst(1);
+    linkedListImp.addFirst(2);
+    linkedListImp.addFirst(3);
+    LinkedList<Integer> linkedList = new LinkedList<>();
+    linkedList.add(0, 1);
+    linkedList.add(0, 2);
+  }
 }
